20f71680df98db0f79dfc32eba60272c18012c32
Raúl Marín <rmarin82@gmail.com>
2022-12-15 00:30:33 +0100

    chore(rollup.config.mjs): add dlls to bundle, add external libraries and fix package name

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,42 +1,68 @@
+import copy from 'rollup-plugin-copy';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 
-const PACKAGE_NAME = 'your-package-name';
+const PACKAGE_NAME = 'ivium';
 const DIST_FOLDER_FILENAME = `dist/${PACKAGE_NAME}`;
+
+const globals = {
+  'ffi-napi': 'ffiNapi',
+  'ref-napi': 'ref',
+  'ref-array-di': 'refArray'
+};
 
 const bundle = (config) => ({
   ...config,
   input: 'src/index.ts',
-  external: (id) => !/^[./]/.test(id),
+  external: [
+    'ffi-napi', 'ref-napi', 'ref-array-di'
+  ],
+  output: config.output.map(outputConfig => ({
+        ...outputConfig,
+        globals,
+        sourcemap: true,
+  }))
 });
+
 
 export default [
   bundle({
     plugins: [esbuild()],
     output: [
       {
+        exports: 'default',
         file: `${DIST_FOLDER_FILENAME}.js`,
         format: 'cjs',
-        sourcemap: true,
       },
       {
+        exports: 'default',
         file: `${DIST_FOLDER_FILENAME}.mjs`,
         format: 'es',
-        sourcemap: true,
       },
       {
+        exports: 'default',
         file: `${DIST_FOLDER_FILENAME}.umd.js`,
         format: 'umd',
         name: PACKAGE_NAME,
-        sourcemap: true,
       },
     ],
   }),
-  bundle({
+  {
+    input: 'dist/ivium.js',
+    plugins: [
+      copy({
+        targets: [
+          { src: 'dlls/*', dest: 'dist/dlls' }
+        ]
+      })
+    ]
+  },
+  {
+    input: 'src/index.ts',
     plugins: [dts()],
     output: {
       file: `${DIST_FOLDER_FILENAME}.d.ts`,
       format: 'es',
     },
-  }),
+  }
 ];
