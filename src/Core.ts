bbfbc04ba24e20ee491a9c3f54ae0cec02005fd3
Raúl Marín <rmarin82@gmail.com>
2022-12-15 14:54:34 +0100

    feat(Core): add IV_open, IV_close and isDriverOpen methods

--- src/Core.ts
+++ src/Core.ts
@@ -10,12 +10,29 @@ const CharArray = ArrayType(char);
 const DLL_PATH = getIviumDllPath();
 console.log(DLL_PATH);
 
-const Core = Library(DLL_PATH, {
-  IV_close: [long, []],
-  IV_connect: [long, [ref.refType(long)]],
-  IV_MaxDevices: [long, []],
-  IV_open: [long, []],
-  IV_readSN: [long, [CharArray]],
-});
+class Core {
+  static #isDriverOpen = false;
+  static #lib = Library(DLL_PATH, {
+    IV_close: [long, []],
+    IV_connect: [long, [ref.refType(long)]],
+    IV_MaxDevices: [long, []],
+    IV_open: [long, []],
+    IV_readSN: [long, [CharArray]],
+  });
+
+  static IV_open() {
+    Core.#isDriverOpen = true;
+    return Core.#lib.IV_open();
+  }
+
+  static IV_close() {
+    Core.#isDriverOpen = false;
+    return Core.#lib.IV_close();
+  }
+
+  static isDriverOpen() {
+    return Core.#isDriverOpen;
+  }
+}
 
 export default Core;
