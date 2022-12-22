851b5cf4b0a97802395c5c32661cf8d6f1b9939b
Raúl Marín <rmarin82@gmail.com>
2022-12-22 13:17:23 +0100

    feat(errors): export all errors from the same place

---
+++ src/errors/index.ts
@@ -0,0 +1,15 @@
+import CellOffError from './CellOffError';
+import DeviceBusyError from './DeviceBusyError';
+import DeviceNotConnectedToIviumsoftError from './DeviceNotConnectedToIviumsoftError';
+import DriverNotOpenError from './DriverNotOpenError';
+import NoDeviceDetectedError from './NoDeviceDetectedError';
+import NoIviumsoftRunningError from './NoIviumsoftRunningError';
+
+export {
+  CellOffError,
+  DeviceBusyError,
+  DeviceNotConnectedToIviumsoftError,
+  DriverNotOpenError,
+  NoDeviceDetectedError,
+  NoIviumsoftRunningError,
+};
