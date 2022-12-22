e2f7224c367118f44a9669ec68fb6c6afc08f979
Raúl Marín <rmarin82@gmail.com>
2022-12-22 13:54:08 +0100

    feat(piviumVerifiers.ts): add verification functions that throw errors when environment is wrong

---
+++ src/piviumVerifiers.ts
@@ -0,0 +1,104 @@
+import Core from './core';
+import {
+  CellOffError,
+  DeviceBusyError,
+  DeviceNotConnectedToIviumsoftError,
+  DriverNotOpenError,
+  IviumsoftNotRunningError,
+} from './errors';
+
+/**
+ * @class
+ * @classdesc Class that encapsulates methods to validated the current status of the IviumSoft environment.
+ */
+class IviumVerifiers {
+  /**
+   * @static
+   * @method
+   * @description Raise exception if the driver is not open.
+   * @throws {DriverNotOpenError}
+   * @memberof IviumVerifiers
+   */
+  static verifiyDriverIsOpen() {
+    if (!Core.isDriverOpen) {
+      throw new DriverNotOpenError();
+    }
+  }
+
+  /**
+   * @static
+   * @method
+   * @description Raise exception if IviumSoft is not running.
+   * @throws {IviumsoftNotRunningError}
+   * @memberof IviumVerifiers
+   */
+  static verifiyIviumsoftIsRunning() {
+    const deviceStatus = Core.IV_getdevicestatus();
+
+    if (deviceStatus === -1) {
+      throw new IviumsoftNotRunningError();
+    }
+  }
+
+  /**
+   * @static
+   * @method
+   * @description Raise exception if a device is not connected through the IviumSoft app.
+   * @throws {DeviceNotConnectedToIviumsoftError}
+   * @memberof IviumVerifiers
+   */
+  static verifyDeviceIsConnectedToIviumsoft() {
+    const deviceStatus = Core.IV_getdevicestatus();
+
+    if (deviceStatus < 1 || deviceStatus === 3) {
+      throw new DeviceNotConnectedToIviumsoftError();
+    }
+  }
+
+  /**
+   * @static
+   * @method
+   * @description Raise exception if no device is connected to your computer through usb.
+   * @throws {DeviceNotConnectedToIviumsoftError}
+   * @memberof IviumVerifiers
+   */
+  static verifyDeviceIsConnectedToComputer() {
+    const deviceStatus = Core.IV_getdevicestatus();
+
+    if (deviceStatus === 3) {
+      throw new DeviceNotConnectedToIviumsoftError();
+    }
+  }
+
+  /**
+   * @static
+   * @method
+   * @description Raise exception if no device is connected to your computer through usb.
+   * @throws {DeviceBusyError}
+   * @memberof IviumVerifiers
+   */
+  static verifyDeviceIsAvailable() {
+    const deviceStatus = Core.IV_getdevicestatus();
+
+    if (deviceStatus === 2) {
+      throw new DeviceBusyError();
+    }
+  }
+
+  /**
+   * @static
+   * @method
+   * @description Raise exception if cell is off.
+   * @throws {CellOffError}
+   * @memberof IviumVerifiers
+   */
+  static verfiyCellIsOn() {
+    const cellStatus = Core.IV_getcellstatus();
+
+    if (!cellStatus.includes(0)) {
+      throw new CellOffError();
+    }
+  }
+}
+
+export default IviumVerifiers;
