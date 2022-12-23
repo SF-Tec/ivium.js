e1fbafe3981ad7874bfcd6a10dbf22d010589f63
Raúl Marín <rmarin82@gmail.com>
2022-12-23 20:26:03 +0100

    feat(ivium.ts): add loadMethod and closeDriver functions

--- src/ivium.ts
+++ src/ivium.ts
@@ -1,16 +1,20 @@
 import Core from './core';
+import FileNotFoundError from './errors/FileNotFoundError';
 import IviumVerifiers from './iviumVerifiers';
 
 /**
  * Wrapper class for the Ivium library.
  * It uses the methods defined in the Core class.
- * All the methods are static.
+ * All its methods are static.
  */
 class Ivium {
   // #######################
   // ## GENERIC FUNCTIONS ##
   // #######################
 
+  /**
+   * Open the driver to manipulate the Ivium software.
+   */
   static openDriver() {
     if (Core.isDriverOpen()) {
       Core.IV_close();
@@ -21,6 +25,42 @@ class Ivium {
     } catch (error) {
       Core.IV_close();
       throw error;
+    }
+  }
+
+  /**
+   * Close the iviumsoft driver.
+   */
+  static closeDriver() {
+    IviumVerifiers.verifiyDriverIsOpen();
+
+    Core.IV_close();
+  }
+
+  // ###########################
+  // ## DIRECT MODE FUNCTIONS ##
+  // ###########################
+
+  // ###########################
+  // ## WE32 MODE FUNCTIONS ##
+  // ###########################
+
+  // ###########################
+  // ## METHOD MODE FUNCTIONS ##
+  // ###########################
+
+  /**
+   * Loads method procedure previously saved to a file.
+   * @param methodFilePath The path to the file where the method is stored.
+   */
+  static loadMethod(methodFilePath: string) {
+    IviumVerifiers.verifiyDriverIsOpen();
+    IviumVerifiers.verifiyIviumsoftIsRunning();
+
+    const [resultCode] = Core.IV_readmethod(methodFilePath);
+
+    if (resultCode === 1) {
+      throw new FileNotFoundError();
     }
   }
 }
