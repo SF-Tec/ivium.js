3dcba116df4510f8ce9c6c31b8b4dc6cbc1c8bdb
Raúl Marín <rmarin82@gmail.com>
2023-01-09 13:48:32 +0100

    fix(index.ts): export the Ivium errors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Core from './core';
 import Ivium from './ivium';
 import IviumVerifiers from './iviumVerifiers';
+import * as IviumErrors from './errors';
 
-export { Core, Ivium, IviumVerifiers };
+export { Core, Ivium, IviumErrors, IviumVerifiers };
