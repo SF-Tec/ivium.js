fd5c5fca2861fefdff71240e77045677d5f1bff0
Raúl Marín <rmarin82@gmail.com>
2023-01-09 17:11:33 +0100

    feat(with-nextjs-trpc): add trpc client/server

--- examples/with-nextjs-trpc/src/pages/_app.tsx
+++ examples/with-nextjs-trpc/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { trpc } from '../utils/trpc';
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default trpc.withTRPC(App);
