5074ba8ab87a852bab086893869a87b9ed8c333d
Alejandro Gutiérrez <npdsolutions@gmail.com>
2023-01-10 12:35:45 +0100

    feat([trpc].js): links iviumjs generic functiond to nextjs API

--- examples/with-nextjs-trpc/src/pages/api/trpc/[trpc].ts
+++ examples/with-nextjs-trpc/src/pages/api/trpc/[trpc].ts
@@ -7,7 +7,17 @@ import { Ivium } from 'iviumjs';
 import { z } from 'zod';
 import { publicProcedure, router } from '../../../server/trpc';
 
-function executeIviumMethod<T extends void | number>(method: () => T) {
+type IviumResult<T extends string | number | number[]> = [number, T];
+
+function executeIviumMethod<
+  T extends
+    | void
+    | number
+    | string
+    | boolean
+    | IviumResult<string | number | number[]>
+    | number[]
+>(method: () => T) {
   try {
     const result = method();
 
@@ -41,17 +51,28 @@ const appRouter = router({
         // 💡 Tip: Try adding a new property here and see it propagate to the client straight-away
       };
     }),
+
+  // #######################
+  // ## GENERIC FUNCTIONS ##
+  // #######################
+
+  openDriver: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.openDriver());
+  }),
   closeDriver: publicProcedure.query(() => {
     return executeIviumMethod(() => Ivium.closeDriver());
   }),
-  connectDevice: publicProcedure.query(() => {
-    return executeIviumMethod(() => Ivium.connectDevice());
+  getMaxDeviceNumber: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.getMaxDeviceNumber());
   }),
-  getPotential: publicProcedure.query(() => {
-    return executeIviumMethod(() => Ivium.getPotential());
+  getDeviceStatus: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.getDeviceStatus());
   }),
-  openDriver: publicProcedure.query(() => {
-    return executeIviumMethod(() => Ivium.openDriver());
+  isIviumsoftRunning: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.isIviumsoftRunning());
+  }),
+  getActiveIviumsoftInstances: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.getActiveIviumsoftInstances());
   }),
   selectIviumsoftInstance: publicProcedure
     .input(
@@ -64,6 +85,35 @@ const appRouter = router({
         Ivium.selectIviumsoftInstance(instanceNumber)
       );
     }),
+  getDeviceSerialNumber: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.getDeviceSerialNumber());
+  }),
+  connectDevice: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.connectDevice());
+  }),
+  disconnectDevice: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.disconnectDevice());
+  }),
+  getDllVersion: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.getDllVersion());
+  }),
+  getIviumsoftVersion: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.getIviumsoftVersion());
+  }),
+  selectChannel: publicProcedure
+    .input(
+      z.object({
+        channelNumber: z.number(),
+      })
+    )
+    .query(({ input: { channelNumber } }) => {
+      return executeIviumMethod(() => Ivium.selectChannel(channelNumber));
+    }),
+
+  getPotential: publicProcedure.query(() => {
+    return executeIviumMethod(() => Ivium.getPotential());
+  }),
+
   // 💡 Tip: Try adding a new procedure here and see if you can use it in the client!
   // getUser: publicProcedure.query(() => {
   //   return { id: '1', name: 'bob' };
