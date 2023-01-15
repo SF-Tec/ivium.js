752087001d0896713e0266b82bf74a095c1f7e47
Raúl Marín <rmarin82@gmail.com>
2023-01-16 00:30:33 +0100

    feat(pages\index): add switch for connecting a device and switching cell on/off

--- examples/with-nextjs-trpc/src/pages/index.tsx
+++ examples/with-nextjs-trpc/src/pages/index.tsx
@@ -1,170 +1,156 @@
-/**
- * This is a Next.js page.
- */
 import { trpc } from '../utils/trpc';
-import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
 import { useState } from 'react';
-import { Space, Switch } from 'antd';
+import { Button, Space, Spin, Switch } from 'antd';
+import { useRouter } from 'next/router';
+import Layout from 'components/Layout';
 
 const { generalIviumFunctions, directModeFunctions } = trpc;
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function IndexPage() {
   const [isDriverOpen, setIsDriverOpen] = useState(false);
   const [isDeviceConnected, setIsDeviceConnected] = useState(false);
-  // 💡 Tip: CMD+Click (or CTRL+Click) on `greeting` to go to the server definition
+  const [isCellOn, setIsCellOn] = useState(false);
+
+  const router = useRouter();
+
   const openDriverMutation = generalIviumFunctions.openDriver.useMutation();
   const closeDriverMutation = generalIviumFunctions.closeDriver.useMutation();
-  // const {mutate: selectIviumsoftInstance} = generalIviumFunctions.selectIviumsoftInstance.useMutation();
+
   const connectDeviceMutation =
     generalIviumFunctions.connectDevice.useMutation();
   const disconnectDeviceMutation =
     generalIviumFunctions.disconnectDevice.useMutation();
+  const setCellOnMutation = directModeFunctions.setCellOn.useMutation();
+  const setCellOffMutation = directModeFunctions.setCellOff.useMutation();
+
+  const openDriver = () => {
+    openDriverMutation.mutate(undefined, {
+      onSuccess: () => {
+        setIsDriverOpen(true);
+      },
+    });
+  };
+
+  const closeDriver = () => {
+    closeDriverMutation.mutate(undefined, {
+      onSuccess: () => {
+        setIsDriverOpen(false);
+      },
+    });
+  };
 
   const isMutationLoading =
     openDriverMutation.isLoading ||
     closeDriverMutation.isLoading ||
     connectDeviceMutation.isLoading ||
-    disconnectDeviceMutation.isLoading;
+    disconnectDeviceMutation.isLoading ||
+    setCellOnMutation.isLoading ||
+    setCellOffMutation.isLoading;
 
-  const { isLoading: isIviumsoftCheckLoading, data: isIviumsoftRunning } =
+  const { data: isIviumsoftRunning, isLoading: isIviumsoftRunningLoading } =
     generalIviumFunctions.isIviumsoftRunning.useQuery(undefined, {
       enabled: isDriverOpen && !isMutationLoading,
-      refetchInterval: 500,
+      refetchInterval: 1000,
     });
 
   const { data: potential, isSuccess: isGetPotentialSuccess } =
     directModeFunctions.getPotential.useQuery(undefined, {
-      enabled: isDriverOpen && !isMutationLoading && isDeviceConnected,
+      enabled:
+        isDriverOpen &&
+        !isMutationLoading &&
+        isDeviceConnected &&
+        isIviumsoftRunning,
       refetchInterval: 2000,
     });
-
-  const areConnectionsButtonsDisabled =
-    !isDriverOpen ||
-    isMutationLoading ||
-    isIviumsoftCheckLoading ||
-    !isIviumsoftRunning;
 
   const hasMutationError =
     openDriverMutation.error ||
     closeDriverMutation.error ||
     connectDeviceMutation.error ||
-    disconnectDeviceMutation.error;
+    disconnectDeviceMutation.error ||
+    setCellOnMutation.error ||
+    setCellOffMutation.error;
+
+  if (isMutationLoading)
+    return (
+      <Layout>
+        <Spin />
+      </Layout>
+    );
+
+  if (isDriverOpen && !isIviumsoftRunningLoading && !isIviumsoftRunning) {
+    closeDriver();
+
+    router.reload();
+  }
+
+  if (isDriverOpen && isIviumsoftRunning)
+    return (
+      <Layout>
+        <Space direction="vertical">
+          <Switch
+            disabled={isMutationLoading || !isDriverOpen || !isIviumsoftRunning}
+            checkedChildren="Disconnect Device"
+            unCheckedChildren="Connect Device"
+            checked={isDeviceConnected}
+            onChange={(checked) => {
+              const connectionMutation = checked
+                ? connectDeviceMutation
+                : disconnectDeviceMutation;
+
+              connectionMutation.mutate(undefined, {
+                onSuccess: () => {
+                  setIsDeviceConnected(checked);
+                },
+              });
+            }}
+          />
+          <Switch
+            disabled={
+              isMutationLoading || !isDeviceConnected || !isIviumsoftRunning
+            }
+            checkedChildren="Set Cell Off"
+            unCheckedChildren="Set Cell On"
+            checked={isCellOn}
+            onChange={(checked) => {
+              const cellMutation = checked
+                ? setCellOnMutation
+                : setCellOffMutation;
+
+              cellMutation.mutate(undefined, {
+                onSuccess: () => {
+                  setIsCellOn(checked);
+                },
+              });
+            }}
+          />
+        </Space>
+        {!hasMutationError && isGetPotentialSuccess && (
+          <div>
+            <h1>Potential</h1>
+            <h2>{potential}</h2>
+          </div>
+        )}
+      </Layout>
+    );
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-      </div>
+    <Layout>
       <Space direction="vertical">
-        <Switch
-          disabled={isMutationLoading || isDeviceConnected}
-          checkedChildren="Close Driver"
-          unCheckedChildren="Open Driver"
-          checked={isDriverOpen}
-          onChange={(checked) => {
-            openDriverMutation.mutate(undefined, {
-              onSuccess: () => {
-                setIsDriverOpen(checked);
-              },
-            });
+        <Button
+          onClick={() => {
+            openDriver();
           }}
-        />
-        <Switch
-          disabled={isMutationLoading || !isDriverOpen || !isIviumsoftRunning}
-          checkedChildren="Disconnect Device"
-          unCheckedChildren="Connect Device"
-          checked={isDeviceConnected}
-          onChange={(checked) => {
-            const connectionMutation = checked
-              ? connectDeviceMutation
-              : disconnectDeviceMutation;
-            connectionMutation.mutate(undefined, {
-              onSuccess: () => {
-                setIsDeviceConnected(checked);
-              },
-            });
-          }}
-        />
+        >
+          Connect App to IviumSoft
+        </Button>
+        {openDriverMutation.error && (
+          <h2>
+            Iviumsoft is not running. Please launch the software and try
+            connecting again.
+          </h2>
+        )}
       </Space>
-      {!hasMutationError && isGetPotentialSuccess && (
-        <div>
-          <h1>Potential</h1>
-          <h2>{potential}</h2>
-        </div>
-      )}
-      {hasMutationError && (
-        <div>
-          <h1>Errors</h1>
-          <h2>{openDriverMutation.error?.message}</h2>
-          <h2>{connectDeviceMutation.error?.message}</h2>
-          <h2>{disconnectDeviceMutation.error?.message}</h2>
-          <h2>{closeDriverMutation.error?.message}</h2>
-        </div>
-      )}
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and&nbsp;API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL
-            with&nbsp;Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </Layout>
   );
 }
