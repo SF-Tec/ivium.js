2e8e403ce80420c946a10e4ce6c0e21d306ae246
Raúl Marín <rmarin82@gmail.com>
2023-01-16 14:23:11 +0100

    feat(ToggleSwitch): add error message to component

--- examples/with-nextjs-trpc/src/components/ToggleSwitch.tsx
+++ examples/with-nextjs-trpc/src/components/ToggleSwitch.tsx
@@ -4,6 +4,7 @@ type ToggleSwitchProps = {
   checked: boolean;
   disabled: boolean;
   label: string;
+  errorText?: string;
   onChange: (checked: boolean) => void;
 };
 
@@ -11,6 +12,7 @@ const ToggleSwitch = ({
   checked,
   disabled,
   label,
+  errorText,
   onChange,
 }: ToggleSwitchProps) => {
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,22 +21,25 @@ const ToggleSwitch = ({
 
   return (
     <div className={styles.container}>
-      <span className={styles['label-text']}>{label}</span>
-      <div className={styles['toggle-switch']}>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          name={label}
-          id={label}
-          checked={checked}
-          disabled={disabled}
-          onChange={handleCheckboxChange}
-        />
-        <label className={styles.label} htmlFor={label}>
-          <span className={styles.inner} />
-          <span className={styles.switch} />
-        </label>
+      <div className={styles['toggle-and-label']}>
+        <span className={styles['label-text']}>{label}</span>
+        <div className={styles['toggle-switch']}>
+          <input
+            checked={checked}
+            className={styles.checkbox}
+            disabled={disabled}
+            id={label}
+            name={label}
+            onChange={handleCheckboxChange}
+            type="checkbox"
+          />
+          <label className={styles.label} htmlFor={label}>
+            <span className={styles.inner} />
+            <span className={styles.switch} />
+          </label>
+        </div>
       </div>
+      {errorText && <span className={styles['error-text']}>{errorText}</span>}
     </div>
   );
 };
